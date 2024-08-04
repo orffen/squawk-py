7716771fd3662f433241d17d8aa4b1f1b1426653
Orffen <orffen@orffenspace.com>
2024-08-04 12:41:52 +1000

    cleanup utils.py with docstrings

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,9 @@ import urllib.request
 
 
 def generate_squawk() -> str:
+    """Generates a valid squawk code, excluding reserved codes."""
     code = "1200"
-    while not squawk_OK(code):
+    while not is_valid_squawk(code):
         code = str(random.randint(0, 7))
         code += str(random.randint(0, 7))
         code += str(random.randint(0, 7))
@@ -12,7 +13,11 @@ def generate_squawk() -> str:
     return code
 
 
-def squawk_OK(code: str) -> bool:
+def is_valid_squawk(code: str) -> bool:
+    """
+    Checks if a squawk code is valid by comparing it to a list of reserved
+    codes.
+    """
     RESERVED_CODES = [
         21, 22, 25, 33, 500, 600, 700, 1200, 5061, 5062, 7001, 7004, 7615,
         *list(range(41, 58)), *list(range(100, 701)), *list(range(1200, 1278)),
@@ -29,6 +34,10 @@ def squawk_OK(code: str) -> bool:
 
 
 def tod_calc_distance(current: int, target: int) -> int:
+    """
+    Calculate the distance required for a 3 degree descent from a current
+    to a target altitude.
+    """
     # work with both '000s of feet or FLs
     current = current if current < 1000 else current / 1000
     target = target if target < 1000 else target / 1000
@@ -38,12 +47,21 @@ def tod_calc_distance(current: int, target: int) -> int:
 
 
 def tod_calc_rate(ground_speed: int) -> int:
+    """
+    Calculate the required descent rate in feet-per-minute for a 3 degree
+    descent.
+    """
     if ground_speed < 0:
         return 0
     return ground_speed * 5
 
 
 def retrieve_metar(icao: str) -> str:
+    """
+    Retrieve the current METAR/TAF information from a given set of ICAO codes.
+
+    Can accept a list of codes separated by commas (e.g. "LOWI,EDDF,").
+    """
     API_URL = "https://aviationweather.gov/api/data/metar?taf=true&ids="
     with urllib.request.urlopen(API_URL + icao) as response:
         if (response.status != 200):
